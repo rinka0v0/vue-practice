613baeb044d0f888139e4c3a54a099f499c77f62
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-10-16 17:29:05 +0900

    新しいページの追加

---
+++ src/views/Todo.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- <div>todo list</div> -->
+  <TodoList />
+</template>
+
+<script lang="ts">
+import { Vue, Options } from "vue-class-component";
+import TodoList from "@/components/TodoList.vue";
+@Options({
+  components: {
+    TodoList,
+  },
+})
+export default class Todo extends Vue {}
+</script>
