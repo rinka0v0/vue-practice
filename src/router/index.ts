931367e21275c0c97cc87196f6f81e0497655a33
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-10-16 17:28:15 +0900

    ルーティングの変更

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+// import Home from "../views/Home.vue";
+import Todo from "../views/Todo.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Todo,
   },
   {
     path: "/about",
